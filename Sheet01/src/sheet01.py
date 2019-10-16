bd6ff168f6cc57c3ec350c5371a7d3ab31ef8deb
Aakash Aggarwal <aakash.srt@gmail.com>
2019-10-16 19:30:15 +0200

    corrected the template and files, also not using Command line args

--- Sheet01/src/sheet01.py
+++ Sheet01/src/sheet01.py
@@ -1,6 +1,9 @@
 import cv2 as cv
 import numpy as np
-
+from matplotlib import pyplot as plt
+import random
+import time
+import sys
 
 def display_image(window_name, img):
     """
@@ -13,17 +16,78 @@ def display_image(window_name, img):
     cv.destroyAllWindows()
 
 
-def integral(img):
+def cal_integral(img):
     int_ = cv.copyMakeBorder(img.copy().astype(np.int64),top=1,left=1,bottom=0,right=0,borderType=cv.BORDER_CONSTANT,value=0)
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
             int_[i+1, j+1] = int_[i, j+1] + int_[i+1,j] - int_[i, j] + img[i, j]
-
     return int_
 
+if __name__ == '__main__':
+    # img_path = sys.argv[1]
+    img_path = "../src/bonn.png"
+    img = cv.imread(img_path, flags=cv.IMREAD_GRAYSCALE)
 
-img = cv.imread('../images/bonn.png', flags=cv.IMREAD_GRAYSCALE)
-# int = cv.integral(img)
-int = integral(img)
-normalise_int = (255.0 * int / int.max()).astype(np.uint8)
-display_image("Integral Img", normalise_int)
+
+#    =========================================================================
+#    ==================== Task 1 =================================
+#    =========================================================================
+    print('Task 1:');
+    # int = cv.integral(img)
+    integral = cal_integral(img)
+    normalise_int = (255.0 * (integral - integral.min() / (integral.max() - integral.min()))).astype(np.uint8)
+    display_image("Integral Img", normalise_int)
+
+
+
+
+#    =========================================================================
+#    ==================== Task 2 =================================
+#    =========================================================================
+    print('Task 2:');
+
+
+
+
+
+#    =========================================================================
+#    ==================== Task 4 =================================
+#    =========================================================================
+    print('Task 4:');
+
+
+
+
+
+#    =========================================================================
+#    ==================== Task 6 =================================
+#    =========================================================================
+    print('Task 6:');
+
+
+
+
+
+#    =========================================================================
+#    ==================== Task 7 =================================
+#    =========================================================================
+    print('Task 7:');
+
+
+
+
+
+#    =========================================================================
+#    ==================== Task 8 =================================
+#    =========================================================================
+    print('Task 8:');
+
+
+
+
+
+
+
+
+
+
