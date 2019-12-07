c2e46df8b2ec6a7dfe17f0134674a50730e4e65a
Kajaree <kajarees@gmail.com>
2019-12-07 23:33:51 +0100

    final draft

--- Sheet07_CVI19-20/task01.py
+++ Sheet07_CVI19-20/task01.py
@@ -4,94 +4,105 @@ import matplotlib.pyplot as plt
 
 image = cv2.imread('data/hand.jpg', 0)
 
+class ICP:
+    def __init__(self, image, landmarks):
+        self.image = image
+        self.landmarks = landmarks
+        self.psi = None
+        self.iterations = 0
+        self.x = None
 
-def get_pixel_locations(landmarks):
-    W = landmarks.T
-    return W[0], W[1]
+    def get_pixel_locations(self):
+        '''
+        map landmarks to pixel co-ordinates
+        :return: pixel co-ordinates
+        '''
+        W = self.landmarks.T
+        return W[0], W[1]
 
-def get_distance_transformed():
-    '''
-    Calculate the distance transformation of the image
-    :return: distance transformation of the image
-    '''
-    edges = cv2.Canny(image, 40, 80)
-    edges[np.where(edges == 255)] = 1.0
-    edges[np.where(edges == 0)] = 255.0
-    edges[np.where(edges == 1)] = 0.0
-    distance_transformed = cv2.distanceTransform(edges, cv2.DIST_L2, 3).astype(np.uint8)
-    return distance_transformed.astype(np.float32)
+    def get_distance_transformed(self):
+        '''
+        Calculate the distance transformation of the image
+        :return: distance transformation of the image
+        '''
+        edges = cv2.Canny(self.image, 40, 80)
+        edges[np.where(edges == 255)] = 1.0
+        edges[np.where(edges == 0)] = 255.0
+        edges[np.where(edges == 1)] = 0.0
+        distance_transformed = cv2.distanceTransform(edges, cv2.DIST_L2, 3).astype(np.uint8)
+        return distance_transformed.astype(np.float32)
 
-def get_transformation(landmarks, image_points):
-    '''
-    Try to find an affine transformation matrix that maps landmarks to the image_points
-    :param landmarks: initial guess of the shape
-    :param image_points:
-    :return:
-    '''
-    dim = landmarks.shape[0]
-    I = np.vstack([np.diag([1., 1.])]*dim)
-    xn = np.array([image_points.T[1], image_points.T[0]]).T.flatten()
-    first = np.zeros((landmarks.shape[0]*2, landmarks.shape[1]))
-    first[::2] = np.fliplr(landmarks)
-    second = np.vstack([[0., 0.], first])[:-1]
-    points = np.hstack([first, second, I])
-    psi = np.dot(np.linalg.pinv(points), xn)
-    correspondence = np.reshape(points @ psi, (int(xn.shape[0] / 2), 2))
-    new_landmarks = np.column_stack([correspondence.T[1], correspondence.T[0]]).astype(int)
-    return psi, new_landmarks
+    def get_transformation(self, landmarks, image_points):
+        '''
+        Try to find an affine transformation matrix that maps landmarks to the image_points
+        :param landmarks: initial guess of the shape
+        :param image_points: corresponding points on the image
+        :return:
+        '''
+        dim = landmarks.shape[0]
+        I = np.vstack([np.diag([1., 1.])]*dim)
+        xn = np.column_stack([image_points.T[1], image_points.T[0]]).flatten()
+        first = np.zeros((landmarks.shape[0]*2, landmarks.shape[1]))
+        first[::2] = np.fliplr(landmarks)
+        second = np.vstack([[0., 0.], first])[:-1]
+        points = np.hstack([first, second, I])
+        psi = np.dot(np.linalg.pinv(points), xn)
+        correspondence = np.reshape(points @ psi, (int(xn.shape[0] / 2), 2))
+        new_landmarks = np.column_stack([correspondence.T[1], correspondence.T[0]]).astype(int)
+        return psi, new_landmarks
 
-def plot_landmarks(landmarks, title, x=None):
-    '''
-    Plot the landmarks on the image
-    :param landmarks: approximation of the points on the shape
-    :param title: Title of the plot
-    :param x: Image points if available
-    :return:
-    '''
-    plt.figure()
-    plt.imshow(image, cmap=plt.cm.gray)
-    if x is not None:
-        plt.scatter(x[:, 1], x[:, 0], c='r', s=10)
-    plt.scatter(landmarks[:, 1], landmarks[:, 0], c='y', s=10)
-    plt.grid()
-    plt.title(title)
-    plt.show()
+    def plot_landmarks(self, landmarks, title, x=None):
+        '''
+        Plot the landmarks on the image
+        :param landmarks: approximation of the points on the shape
+        :param title: Title of the plot
+        :param x: Image points if available
+        :return:
+        '''
+        plt.figure()
+        plt.imshow(image, cmap=plt.cm.gray)
+        if x is not None:
+            plt.scatter(x[:, 1], x[:, 0], c='r', s=10)
+        plt.scatter(landmarks[:, 1], landmarks[:, 0], c='y', s=10)
+        plt.grid()
+        plt.title(title)
+        plt.show()
 
-def ICP(W, epsilon=0.001):
-    '''
-    Try to map the W to the closest point on the edge of the shape
-    :param D: distance Transformed image
-    :param W: initial landmarks points
-    :param epsilon: covergence threshold
-    :return: the final landmark points closest to the shape
-    '''
-    D = get_distance_transformed()
-    G_y, G_x = np.gradient(D)
-    delta = False
-    wn_old = W
-    count = 0
-    psi_old = None
-    while not delta:
-        X, Y = get_pixel_locations(wn_old)
-        Gx = G_x[X, Y]
-        Gy = G_y[X, Y]
-        gradient = np.column_stack([Gy, Gx])
-        Dn = D[X, Y]
-        denominator = np.hypot(Gy, Gx).reshape(-1, 1)
-        numerator = Dn.reshape(-1, 1) * gradient
-        xn_new = wn_old - np.divide(numerator, denominator, where=denominator != 0)
-        psi, wn_new = get_transformation(wn_old, xn_new.astype('int'))
-        plot_landmarks(wn_new.astype('int'), 'Iteration: {}'.format(count+1), xn_new.astype('int'))
-        count += 1
-        wn_old = wn_new.astype('int')
-        delta = np.array_equal(psi, psi_old)
-        psi_old = psi
-    return wn_old
+    def run_ICP(self, generate_plot=True):
+        '''
+        Try to map the W to the closest point on the edge of the shape
+        :param generate_plot: Boolean flag for plotting
+        :return: the final landmark points closest to the shape
+        '''
+        D = self.get_distance_transformed()
+        G_y, G_x = np.gradient(D)
+        delta = False
+        wn_old = self.landmarks
+        while not delta:
+            X, Y = self.get_pixel_locations()
+            Gx = G_x[X, Y]
+            Gy = G_y[X, Y]
+            gradient = np.column_stack([Gy, Gx])
+            Dn = D[X, Y]
+            denominator = np.hypot(Gy, Gx).reshape(-1, 1)
+            numerator = Dn.reshape(-1, 1) * gradient
+            xn_new = self.landmarks - np.divide(numerator, denominator, where=denominator != 0)
+            psi, wn_new = self.get_transformation(self.landmarks, xn_new.astype('int'))
+            if generate_plot:
+                self.plot_landmarks(wn_new.astype('int'), 'Iteration: {}'.format(self.iterations+1), xn_new.astype('int'))
+            self.iterations += 1
+            self.landmarks = wn_new.astype('int')
+            delta = np.array_equal(psi, self.psi)
+            self.psi = psi
+            self.x = xn_new.astype('int')
+        return self.landmarks
 
 
-with open("data/hand_landmarks.txt", "r") as file:
-    raw_data = [tuple(map(int, line[1:-2].split(','))) for line in file]
-landmarks_data = np.array(raw_data).T
-landmarks = np.column_stack([landmarks_data[1], landmarks_data[0]])
-new_landmarks = ICP(landmarks)
+if __name__=='__main__':
+    with open("data/hand_landmarks.txt", "r") as file:
+        raw_data = [tuple(map(int, line[1:-2].split(','))) for line in file]
+    landmarks_data = np.array(raw_data).T
+    landmarks = np.column_stack([landmarks_data[1], landmarks_data[0]])
+    correspondence = ICP(image, landmarks)
+    new_landmarks = correspondence.run_ICP()
 
--- Sheet07_CVI19-20/task02.py
+++ Sheet07_CVI19-20/task02.py
@@ -1,8 +1,58 @@
-import cv2 as cv
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
-from task01 import ICP, get_transformation, plot_landmarks
+
+def apply_Tinv(x, psi):
+    x = x.T.reshape(2, 1)
+    R = psi[:4].reshape(2, 2)
+    t = psi[-2:].reshape(2, 1)
+    return np.dot(np.linalg.inv(R), (x - t))
+
+
+def get_transformation(landmarks, points):
+    '''
+    Try to find an affine transformation matrix that maps landmarks to the image_points
+    :param landmarks: initial guess of the shape
+    :param image_points: corresponding points on the image
+    :return:
+    '''
+    dim = landmarks.shape[0]
+    I = np.vstack([np.diag([1., 1.])] * dim)
+    xn = np.column_stack([points.T[0], points.T[1]]).flatten()
+    first = np.zeros((landmarks.shape[0] * 2, landmarks.shape[1]))
+    first[::2] = np.fliplr(landmarks)
+    second = np.vstack([[0., 0.], first])[:-1]
+    points = np.hstack([first, second, I])
+    psi = np.dot(np.linalg.pinv(points), xn)
+    correspondence = np.reshape(points @ psi, (int(xn.shape[0] / 2), 2))
+    new_landmarks = np.column_stack([correspondence.T[0], correspondence.T[1]]).astype(int)
+    return psi, new_landmarks
+
+def run_ICP(landmarks, points, generate_plot=True):
+    '''
+    Try to map the W to the closest point on the edge of the shape
+    :param generate_plot: Boolean flag for plotting
+    :return: the final landmark points closest to the shape
+    '''
+    delta = False
+    prev_psi = None
+    iterations = 0
+    wn_old = landmarks
+    new_landmarks = None
+    while not delta:
+        psi, wn_new = get_transformation(wn_old, points.astype('int'))
+        iterations += 1
+        wn_old = wn_new.astype('int')
+        if prev_psi is not None:
+            delta = np.array_equal(psi, prev_psi)
+        prev_psi = psi
+    return prev_psi, wn_old
+
+def get_landmark_points(points):
+    x = points[:int(points.shape[0] / 2)]
+    y = points[int(points.shape[0] / 2):]
+    return np.column_stack([y, x])
 
 def pca(centered_data, threshold=0.1):
     wwt = centered_data @ centered_data.T
@@ -19,22 +69,54 @@ def plot_shape(points, title):
     plt.title(title)
     plt.show()
 
-training_data = np.loadtxt('data/hands_aligned_train.txt.new', skiprows=1)
-weights = np.array([(-0.4, -0.2, 0.0, 0.2, 0.4)])
-mu = np.mean(training_data, axis=1)
-plot_shape(mu, 'Mean Shape')
-centered_data = training_data - mu.reshape(112, 1)
-U, L2, K = pca(centered_data)
-LK = np.diag(L2[:K])
-UK = U[:K, :K]
-variance_hat = np.sum(L2[K:])/(L2.shape[0] - K)
-I = np.eye(LK.shape[0])
-phi_hat = UK @ np.sqrt(np.subtract(LK, variance_hat*I))
-prev_weights = weights
-for i in range(K):
-    weights_str = ",".join(map(str, prev_weights.tolist()))
-    hphi = phi_hat * prev_weights
-    w = mu + np.sum(hphi)
-    plot_shape(w, 'K = {}, weights = {}'.format(i+1, weights_str))
-    prev_weights = np.roll(prev_weights, 1)
+def task_2():
+    training_data = np.loadtxt('data/hands_aligned_train.txt.new', skiprows=1)
+    weights = np.array([(-0.4, -0.2, 0.0, 0.2, 0.4)])
+    mu = np.mean(training_data, axis=1)
+    #plot_shape(mu, 'Mean Shape')
+    centered_data = training_data - mu.reshape(112, 1)
+    U, L2, K = pca(centered_data)
+    LK = np.diag(L2[:K])
+    UK = U[:, :K]
+    variance_hat = np.sum(L2[K:]) / (L2.shape[0] - K)
+    I = np.eye(LK.shape[0])
+    phi_hat = UK @ np.sqrt(np.subtract(LK, variance_hat * I))
+    prev_weights = weights
+    for i in range(K):
+        weights_str = ",".join(map(str, prev_weights.tolist()))
+        hphi = phi_hat @ prev_weights.T
+        w = mu + np.sum(hphi, axis=0)
+        #plot_shape(w, 'K = {}, weights = {}'.format(i + 1, weights_str))
+        prev_weights = np.roll(prev_weights, 1)
+    return mu, variance_hat, phi_hat
+
+def task_3(mean_shape, phi):
+    b = None
+    hphi = None
+    delta = False
+    test_data = np.loadtxt('data/hands_aligned_test.txt.new', skiprows=1)
+    iterations = 0
+    current_landmark = get_landmark_points(mean_shape)
+    b_old = None
+    points = get_landmark_points(test_data)
+    y = points
+    while not delta:
+        psi, current_landmark = run_ICP(current_landmark, y, generate_plot=False)
+        y = np.apply_along_axis(apply_Tinv, 1, points, psi)
+        y = y.reshape(y.shape[0], y.shape[1])
+        b = phi.T @ np.subtract(y.flatten('F'), mean_shape)
+        if b_old is not None:
+            delta = np.array_equal(b, b_old)
+        hphi = np.dot(phi, np.diag(b))
+        current_landmark = mean_shape + np.sum(hphi)
+        plot_shape(current_landmark.astype('int'), 'Iteration: {}'.format(iterations+1))
+        current_landmark = get_landmark_points(current_landmark)
+        iterations += 1
+        b_old = b
+
+
+if __name__=='__main__':
+    mean_shape, sigma_sqr, phi = task_2()
+    final_shape = task_3(mean_shape.astype('int'), phi)
+
 
