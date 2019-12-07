51918deadd41198067d6d3443a2ece600c32ceac
Kajaree <kajarees@gmail.com>
2019-12-07 12:12:39 +0100

    Task02 final

--- Sheet07_CVI19-20/task01.py
+++ Sheet07_CVI19-20/task01.py
@@ -4,55 +4,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
-dx = np.array([[-1 / 2, 0, 1 / 2]], 'float64')
-
-def get_exponent(x, y, sigma):
-    return -1 * (x * x + y * y) / (2 * sigma)
-
-def get_derivative_of_gaussian_kernel(size, sigma):
-    assert size > 0 and size % 2 == 1 and sigma > 0
-
-    kernel_x = np.zeros((size, size))
-    kernel_y = np.zeros((size, size))
-
-    size_half = size // 2
-
-    for i in range(size):
-        y = i - size_half
-        for j in range(size):
-            x = j - size_half
-            kernel_x[i, j] = (
-                -1
-                * (x / (2 * np.pi * sigma * sigma))
-                * np.exp(get_exponent(x, y, sigma))
-            )
-            kernel_y[i, j] = (
-                -1
-                * (y / (2 * np.pi * sigma * sigma))
-                * np.exp(get_exponent(x, y, sigma))
-            )
-
-    return kernel_x, kernel_y
+image = cv2.imread('data/hand.jpg', 0)
 
 def get_transformation(landmarks, image_points):
+    '''
+    Try to find an affine transformation matrix that maps landmarks to the image_points
+    :param landmarks: initial guess of the shape
+    :param image_points:
+    :return:
+    '''
     dim = landmarks.shape[0]
     I = np.vstack([np.diag([1., 1.])]*dim)
     xn = np.expand_dims(image_points, axis=-1).reshape(image_points.shape[0]*2, 1)
-    #print(xn)
     first = np.zeros((landmarks.shape[0]*2, landmarks.shape[1]))
     first[::2] = landmarks
     second = np.vstack([[0., 0.], first])[:-1]
     points = np.hstack([first, second, I])
-    #print(points.shape)
-    psi = np.linalg.pinv(points) @ xn
+    inv_points = np.dot(np.linalg.inv(points.T @ points), points.T)
+    psi = inv_points @ xn
     return psi, points @ psi
 
-def plot_landmarks(image, landmarks):
+def plot_landmarks(landmarks, title, x=None):
+    '''
+    Plot the landmarks on the image
+    :param landmarks: approximation of the points on the shape
+    :param title: Title of the plot
+    :param x: Image points if available
+    :return:
+    '''
     plt.figure()
-    plt.imshow(image)
+    plt.imshow(image, cmap=plt.cm.gray)
+    if x is not None:
+        plt.scatter(x[:, 0], x[:, 1], c='r', s=10)
     plt.scatter(landmarks[:, 0], landmarks[:, 1], c='y', s=10)
     plt.grid()
+    plt.title(title)
     plt.show()
+
+def ICP(D, W, epsilon=0.1):
+    '''
+    Try to map the W to the closest point on the edge of the shape
+    :param D: distance Transformed image
+    :param W: initial landmarks points
+    :param epsilon: covergence threshold
+    :return: the final landmark points closest to the shape
+    '''
+    G = np.gradient(D)
+    delta = 1
+    wn_old = W
+    count = 0
+    while delta > epsilon:
+        print(count)
+        G_x = G[1][wn_old[:, 1], wn_old[:, 0]]
+        G_y = G[0][wn_old[:, 1], wn_old[:, 0]]
+        gradient = np.column_stack([G_y, G_x])
+        Dn = D[wn_old[:, 1], wn_old[:, 0]]
+        denominator = np.hypot(G_y, G_x)[:, None]
+        multipliers = Dn[:, None] / denominator
+        xn_new = wn_old - np.multiply(multipliers, gradient)
+        psi, wn_new = get_transformation(wn_old, xn_new)
+        wn_new = np.reshape(wn_new, (int(wn_new.shape[0] / 2), wn_new.shape[1] * 2))
+        plot_landmarks(wn_new.astype('int'), 'Iteration: {}'.format(count+1), xn_new.astype('int'))
+        count += 1
+        delta = np.mean(Dn)
+        wn_old = wn_new.astype('int')
+    return wn_old
 
 rgx = re.compile('[%s]' % "(,)")
 with open("data/hand_landmarks.txt", "r") as f:
@@ -61,51 +77,21 @@ X, Y = [], []
 for line in lines:
     line = rgx.sub(' ', line).lstrip().rstrip().split()
     X.append(int(line[0]))
-    Y.append(int(line[-1]))
+    Y.append(int(line[1]))
 landmarks = np.column_stack([X, Y])
-image = plt.imread('data/hand.jpg')
-print(image.shape)
-#plot_landmarks(image, landmarks)
-image_blurred = cv2.GaussianBlur(image, ksize=(5, 5), sigmaX=0.0)
-edges = cv2.Canny(image_blurred, 100, 250, 3)
-plt.imshow(edges, cmap="gray")
-plt.show()
-edges = np.float64(edges) / 255.0
-edges[np.where(edges <= 0.7)] = 0.0
-edges[np.where(edges > 0.7)] = 1.0
-#print(edges)
+plot_landmarks(landmarks, 'Given Landmarks')
 
-edges = 1 - edges  # this is because of the input of the cv2.distanceTransform.
-#print(edges)
+
+image_blurred = cv2.GaussianBlur(image, ksize=(5, 5), sigmaX=0.0)
+edges = cv2.Canny(image_blurred, 60, 150, L2gradient=True)
+edges = np.float64(edges)
+edges = 255.0 - edges  # this is because of the input of the cv2.distanceTransform.
 distance_transformed = cv2.distanceTransform(
-    src=np.uint8(edges * 255.0),
+    src=np.uint8(edges),
     distanceType=cv2.DIST_L2,
     maskSize=cv2.DIST_MASK_PRECISE,
 )
-#print(distance_transformed)
-epsilon = 0.1
-G = np.gradient(distance_transformed)
-G_x = cv2.filter2D(distance_transformed, -1, dx)[Y, X]
-G_y = cv2.filter2D(distance_transformed, -1, dx.T)[Y, X]
+plt.imshow(distance_transformed, cmap="gray")
+plt.show()
+new_landmarks = ICP(distance_transformed, landmarks)
 
-delta = 1
-denominator = np.hypot(G_x, G_y)[:, None]
-Dn = distance_transformed[Y, X][:, None]
-wn_old = landmarks.astype('float64')
-gradient = np.column_stack([G_y, G_x])
-count = 0
-while delta > epsilon:
-    print(count)
-    numerator = Dn * gradient
-    xn_new = wn_old - numerator/denominator
-    print(xn_new)
-    plot_landmarks(image, xn_new.astype('int'))
-    psi, wn_new = get_transformation(wn_old, xn_new)
-    wn_new = np.reshape(wn_new, (int(wn_new.shape[0]/2), wn_new.shape[1]*2))
-    new_landmark = wn_new.astype('int')
-    print(new_landmark)
-    Dn = distance_transformed[new_landmark[:, 0], new_landmark[:, 1]][:, None]
-    #plot_landmarks(image, new_landmark)
-    count += 1
-    delta = np.mean(wn_new - wn_old)
-    wn_old = wn_new
--- Sheet07_CVI19-20/task02.py
+++ Sheet07_CVI19-20/task02.py
@@ -2,52 +2,39 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
+from task01 import ICP, plot_landmarks
 
 def pca(centered_data, threshold=0.1):
     wwt = centered_data @ centered_data.T
     U, L2, Vt = np.linalg.svd(wwt)
     L2_ratio = np.cumsum(L2) / np.sum(L2)
-    compared = L2_ratio[L2_ratio > 1 - threshold]
-    return U, L2, 5
+    compared = L2_ratio[L2_ratio <= 1 - threshold]
+    return U, L2, compared.size
 
-raw_data = np.loadtxt('data/hands_aligned_train.txt.new', skiprows=1)
-training_data = np.zeros()
-training_data[:, ::2] = x
-training_data[:, 1::2] = y
-image = plt.imread('data/hand.jpg')
+def plot_shape(points, title):
+    fig = plt.figure(figsize=(16, 8))
+    x = points[:int(points.shape[0] / 2)]
+    y = points[int(points.shape[0] / 2):]
+    plt.plot(x, y, '-o', color='red', zorder=2)
+    plt.title(title)
+    plt.show()
+
+training_data = np.loadtxt('data/hands_aligned_train.txt.new', skiprows=1)
 weights = np.array([(-0.4, -0.2, 0.0, 0.2, 0.4)])
-mu = np.mean(training_data, axis=0)
-centered_data = training_data - mu
-#print(np.mean(centered_data, axis=0))
-'''points = training_data
-x = points[:int(points.shape[0] / 2)]
-y = points[int(points.shape[0] / 2):]
-plt.figure()
-plt.imshow(image)
-plt.scatter(x, y, c='r', s=40)
-plt.show()'''
-
-
+mu = np.mean(training_data, axis=1)
+plot_shape(mu, 'Mean Shape')
+centered_data = training_data - mu.reshape(112, 1)
 U, L2, K = pca(centered_data)
 LK = np.diag(L2[:K])
-UK = U[:, :K]
+UK = U[:K, :K]
 variance_hat = np.sum(L2[K:])/(L2.shape[0] - K)
 I = np.eye(LK.shape[0])
 phi_hat = UK @ np.sqrt(np.subtract(LK, variance_hat*I))
-print(phi_hat[:K, :K].shape)
-w = mu + phi_hat[:K, :K] @ weights.T
-fig = plt.figure(figsize=(16, 8))
-ax1 = fig.add_subplot(121)
-ax2 = fig.add_subplot(122)
-count = 0
-for i in range(w.shape[1]):
-    points = training_data[:, i]
-    x = points[:int(points.shape[0]/2)]
-    print(x.shape)
-    y = points[int(points.shape[0]/2):]
-    print(y.shape)
-    ax1.clear()
-    ax1.imshow(image, cmap='gray')
-    ax1.scatter(x, y, color='red', s=3)
-    plt.pause(0.01)
-plt.show()
+prev_weights = weights
+for i in range(K):
+    weights_str = ",".join(map(str, prev_weights.tolist()))
+    hphi = phi_hat * prev_weights
+    w = mu + np.sum(hphi)
+    plot_shape(w, 'K = {}, weights = {}'.format(i+1, weights_str))
+    prev_weights = np.roll(prev_weights, 1)
+
