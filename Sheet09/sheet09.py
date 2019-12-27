c14d2cf1d67855d5bebe5fddad64b139098ea720
Kajaree <kajarees@gmail.com>
2019-12-27 16:10:46 +0100

    Solution for optical flow

--- Sheet09/sheet09.py
+++ Sheet09/sheet09.py
@@ -1,7 +1,6 @@
-import numpy as np
 import os
-import time
 import cv2 as cv
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -55,16 +54,37 @@ class OpticalFlow:
     # function for converting flow map to to BGR image for visualisation
     # return bgr image
     def flow_map_to_bgr(self, flow):
-        flow_bgr = None
-
+        flow_img = np.zeros((self.next.shape[0], self.next.shape[1], 3), dtype=np.uint8)
+        flow_img[..., 1] = 255
+        magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1], angleInDegrees=False)
+        flow_img[..., 0] = angle /2
+        flow_img[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
+        flow_bgr = cv.cvtColor(flow_img, cv.COLOR_HSV2BGR)
         return flow_bgr
 
     #***********************************************************************************
     # implement Lucas-Kanade Optical Flow 
     # returns the Optical flow based on the Lucas-Kanade algorithm and visualisation result
     def Lucas_Kanade_flow(self):
-        flow = None
-
+        kernel = np.ones((self.WINDOW_SIZE))
+        rows, cols = self.next.shape
+        flow = np.zeros((rows, cols, 2), dtype=np.float32)
+        IxIx = cv.filter2D(np.square(self.Ix), -1, kernel)
+        IyIy = cv.filter2D(np.square(self.Iy), -1, kernel)
+        IxIy = cv.filter2D(np.multiply(self.Ix, self.Iy), -1, kernel)
+        IxIt = cv.filter2D(np.multiply(self.Ix, self.It), -1, kernel)
+        IyIt = cv.filter2D(np.multiply(self.Iy, self.It), -1, kernel)
+        A = np.dstack((IxIx, IxIy, IxIy, IyIy))
+        B = np.dstack((-IxIt, -IyIt))
+        for row in range(rows):
+            for col in range(cols):
+                a = A[row, col].reshape((2, 2))
+                b = B[row, col]
+                if np.min(np.linalg.eigvals(a)) < self.EIGEN_THRESHOLD:
+                    print('Invalid flow encountered at pixel ({}, {})'.format(row, col))
+                    continue
+                #flow[row, col, :] = np.dot(np.linalg.pinv(a), b)[0]
+                flow[row, col, :] = np.linalg.lstsq(a, b, rcond=None)[0]
         flow_bgr = self.flow_map_to_bgr(flow)
         return flow, flow_bgr
 
@@ -72,8 +92,29 @@ class OpticalFlow:
     # implement Horn-Schunck Optical Flow 
     # returns the Optical flow based on the Horn-Schunck algorithm and visualisation result
     def Horn_Schunck_flow(self):
-        flow = None
-
+        rows, cols = self.next.shape
+        flow = np.zeros((rows, cols, 2), dtype=np.float32)
+        u = np.zeros((rows, cols))
+        v = np.zeros((rows, cols))
+        kernel = np.array([[0., 1./4, 0.], [1./4, -1., 1./4], [0., 1./4, 0.]])
+        iterator = 0
+        delta = 1
+        denominator = self.ALPHA**2 + np.square(self.Ix) + np.square(self.Iy)
+        while delta > self.EPSILON and iterator < self.MAX_ITERS:
+            delta_u = cv.filter2D(u, -1, kernel)
+            delta_v = cv.filter2D(v, -1, kernel)
+            u_hat = u + delta_u
+            v_hat = v + delta_v
+            numarator = np.multiply(self.Ix, u_hat) + np.multiply(self.Iy, v_hat) + self.It
+            update = np.divide(numarator, denominator)
+            u_old = u.copy()
+            v_old = v.copy()
+            u = u_hat - np.multiply(self.Ix, update)
+            v = v_hat - np.multiply(self.Iy, update)
+            delta = np.linalg.norm(u - u_old) + np.linalg.norm(v - v_old)
+            iterator += 1
+        flow[:, :, 0] = u
+        flow[:, :, 1] = v
         flow_bgr = self.flow_map_to_bgr(flow)
         return flow, flow_bgr
 
@@ -81,9 +122,14 @@ class OpticalFlow:
     #calculate the angular error here
     # return average angular error and per point error map
     def calculate_angular_error(self, estimated_flow, groundtruth_flow):
-        aae = None
-        aae_per_point = None
-
+        uc = groundtruth_flow[:, :, 0]
+        vc = groundtruth_flow[:, :, 1]
+        u = estimated_flow[:, :, 0]
+        v = estimated_flow[:, :, 1]
+        numarator = np.multiply(uc, u) + np.multiply(vc, v) + 1
+        denominator = np.sqrt(np.multiply((uc**2 + vc**2 + 1), (u**2 + v**2 + 1)))
+        aae_per_point = (180./np.pi)*np.arccos(np.divide(numarator, denominator))
+        aae = np.sum(aae_per_point) / aae_per_point.size
         return aae, aae_per_point
 
 
@@ -112,14 +158,13 @@ if __name__ == "__main__":
         flow_lucas_kanade, flow_lucas_kanade_bgr = Op.Lucas_Kanade_flow()
         aae_lucas_kanade, aae_lucas_kanade_per_point = Op.calculate_angular_error(flow_lucas_kanade, groundtruth_flow)
         print('Average Angular error for Luacas-Kanade Optical Flow: %.4f' %(aae_lucas_kanade))
-
+        #
         flow_horn_schunck, flow_horn_schunck_bgr = Op.Horn_Schunck_flow()
-        aae_horn_schunk, aae_horn_schunk_per_point = Op.calculate_angular_error(flow_horn_schunck, groundtruth_flow)        
-        print('Average Angular error for Horn-Schunck Optical Flow: %.4f' %(aae_horn_schunk))   
+        aae_horn_schunk, aae_horn_schunk_per_point = Op.calculate_angular_error(flow_horn_schunck, groundtruth_flow)
+        print('Average Angular error for Horn-Schunck Optical Flow: %.4f' %(aae_horn_schunk))
 
         flow_bgr_gt = Op.flow_map_to_bgr(groundtruth_flow)
-
-        fig = plt.figure(figsize=(img.shape))
+        fig = plt.figure()
 
         # Display
         fig.add_subplot(2, 3, 1)
