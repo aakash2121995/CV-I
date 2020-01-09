d8d5f0485745685919471bc17a79592f35066bef
Kajaree <kajarees@gmail.com>
2020-01-09 17:38:27 +0100

    Solution for task 1, 2, 3 and 4

--- Sheet10/src/template.py
+++ Sheet10/src/template.py
@@ -20,43 +20,98 @@ for ptIdx in range(0, board_n):
     obj.append(np.array([[ptIdx/board_w, ptIdx%board_w, 0.0]],np.float32))
 obj = np.vstack(obj)
 
-def task1():
-    #implement your solution
-    pass
+def show_images(images, cols=7, rows=2):
+    fig = plt.figure(figsize=(50, 50))
+    for i in range(NUM_IMAGES):
+        a = fig.add_subplot(cols, np.ceil(NUM_IMAGES / float(cols)), i + 1)
+        plt.imshow(images[i])
+        plt.xticks([])
+        plt.yticks([])
+    #fig.set_size_inches(np.array(fig.get_size_inches()) * NUM_IMAGES)
+    plt.show()
 
-def task2(imagePoints, objectPoints):
+def task1(images):
     #implement your solution
-    pass
+    # termination criteria
+    subpix_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    objectPoints, imagePoints = [], []
+    final_images = []
+    for img in images:
+        image = img.copy()
+        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        retval, corners = cv2.findChessboardCorners(image_gray, board_size, None)
+        # If found, add object points, image points (after refining them)
+        if retval:
+            objectPoints.append(obj)
+            # Refining corners position with sub-pixels based algorithm
+            subpix_corners = cv2.cornerSubPix(image_gray, corners, (3, 3), (-1, -1), subpix_criteria)
+            imagePoints.append(subpix_corners)
+            final_image = cv2.drawChessboardCorners(image, board_size, subpix_corners, retval)
+            final_images.append(final_image)
+            display = np.hstack((img, final_image))
+            cv2.imshow('original --> With_corners', display)
+            cv2.waitKey(500)
+            cv2.destroyAllWindows()
+        else:
+            print('Chessboard not detected in image ')
+    #show_images(final_images)
+    return imagePoints, objectPoints
+
+def task2(imagePoints, objectPoints, image_shape):
+    _, cameraMatrix, distortions, rotation, translation \
+        = cv2.calibrateCamera(objectPoints, imagePoints, image_shape[::-1], None, None)
+    return cameraMatrix, distortions, rotation, translation
 
 def task3(imagePoints, objectPoints, CM, D, rvecs, tvecs):
-    #implement your solution
-    pass
+    projection_error = 0
+    for i in range(len(objectPoints)):
+        newImgpoints, _ = cv2.projectPoints(objectPoints[i], rvecs[i], tvecs[i], CM, D)
+        projection_error += cv2.norm(imagePoints[i], newImgpoints, cv2.NORM_L2) / len(newImgpoints)
+    projection_error /= len(objectPoints)
+    print('Re-projection error: {}'.format(projection_error))
 
-def task4(CM, D):
-    #implement your solution
-    pass
+def task4(images, CM, D):
+    undistorted_images = []
+    for image in images:
+        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        undistorted = cv2.undistort(image_gray, CM, D, None, None)
+        undistorted_images.append(undistorted)
+        display = np.hstack((image_gray, undistorted))
+        cv2.imshow('Distorted --> Undistorted', display)
+        cv2.waitKey(500)
+        cv2.destroyAllWindows()
 
 def task5(CM, rvecs, tvecs):
     #implement your solution
     pass
 
 def main():
+    images = []
     #Showing images
     for img_file in images_files_list:
         print(img_file)
         img = cv2.imread(img_file)
-        cv2.imshow("Task1", img)
-        cv2.waitKey(10)
+        image_shape = img.shape[:2]
+        images.append(img)
+        #cv2.imshow("Task1", img)
+        #cv2.waitKey(10)
+    #show_images(images)
+    imagePoints, objectPoints = task1(images) #Calling Task 1
     
-    imagePoints, objectPoints = task1() #Calling Task 1
-    
-    CM, D, rvecs, tvecs = task2(imagePoints, objectPoints) #Calling Task 2
+    CM, D, rvecs, tvecs = task2(imagePoints, objectPoints, image_shape) #Calling Task 2
+
+    print('Camera matrix:\n {}'.format(CM))
+    print('distortion matrix:\n {}'.format(D))
+    for i in range(len(rvecs)):
+        print('for image - {}:'.format(i + 1))
+        print('rotation vector:\n {}'.format(rvecs[i]))
+        print('translation vector:\n {}'.format(tvecs[i]))
 
     task3(imagePoints, objectPoints, CM, D, rvecs, tvecs)  # Calling Task 3
 
-    task4(CM, D) # Calling Task 4
+    task4(images, CM, D) # Calling Task 4
 
-    task5(CM, rvecs, tvecs) # Calling Task 5
+    #task5(CM, rvecs, tvecs) # Calling Task 5
     
     print("FINISH!")
 
